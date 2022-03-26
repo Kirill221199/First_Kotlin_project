34de25a0cd181ddf0d5bb99c3f7df06f7a8fd2c0
Кирилл <agarin221199@mail.ru>
2022-03-26 16:56:14 +0500

    Add data class

---
+++ app/src/main/java/ru/kirill/first_kotlin_project/FirstDataClass.kt
@@ -0,0 +1,7 @@
+package ru.kirill.first_kotlin_project
+
+data class FirstDataClass(val name:String, val surname:String){
+
+    val age:Int = 22
+}
+
--- app/src/main/java/ru/kirill/first_kotlin_project/MainActivity.kt
+++ app/src/main/java/ru/kirill/first_kotlin_project/MainActivity.kt
@@ -1,8 +1,10 @@
 package ru.kirill.first_kotlin_project
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -28,6 +30,27 @@ class MainActivity : AppCompatActivity() {
         val buttonTwo: Button = findViewById(R.id.button_rus)
         buttonTwo.setOnClickListener(View.OnClickListener {
             myToast("Привет Мир!")
+        })
+
+        val name:TextView = findViewById(R.id.name)
+        val surname:TextView = findViewById(R.id.surname)
+        val age:TextView = findViewById(R.id.age)
+        val buttonSet:Button = findViewById(R.id.button_set_field)
+        val myDataClass = FirstDataClass("Kirill","Agarin")
+        buttonSet.setOnClickListener(View.OnClickListener {
+            name.text = myDataClass.name
+            surname.text = myDataClass.surname
+            age.text = myDataClass.age.toString()
+
+        })
+
+        val nameCopy:TextView = findViewById(R.id.name_copy)
+        val surnameCopy:TextView = findViewById(R.id.surname_copy)
+        val buttonCopy:Button = findViewById(R.id.button_copy_fields)
+        buttonCopy.setOnClickListener(View.OnClickListener {
+            val  myCopyClass = myDataClass.copy()
+            nameCopy.text = myCopyClass.name
+            surnameCopy.text = myCopyClass.surname
         })
 
     }
